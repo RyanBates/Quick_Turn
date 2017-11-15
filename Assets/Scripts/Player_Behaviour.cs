3d7814715c2cb5a0ca1af343d22213db87a6ea89
Ryan Bates <rbates214@gmail.com>
2017-11-15 16:43:13 -0600

    attempted to make a character do a quick turn

---
+++ Assets/Scripts/Player_Behaviour.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Behaviour : MonoBehaviour
+{
+    public Player_Stats character;
+
+    float health;
+    public float speed;
+
+    float horizontal;
+    float vertical;
+
+    public bool moving = false;
+    public bool quickTurn;
+
+    void Start()
+    {
+        health = character.health;
+    }
+
+    void Update()
+    {
+        character.position = transform.position;
+        speed = character.speed;
+
+        //if (speed <= 0.0f)
+        //    moving = false;
+
+        //if (moving == true)
+        //    speed += character.acceleration;
+
+        //if (moving == false)
+        //{
+        //    speed -= character.acceleration;
+        //    speed = 0;
+        //}
+
+        if (speed <= character.max_Speed)
+            if(moving == true)
+                character.speed += character.acceleration;
+
+        if (speed >= character.max_Speed)
+        {
+            speed = character.max_Speed;
+        }
+
+
+
+        horizontal += Input.GetAxis("Horizontal") * speed;
+        vertical += Input.GetAxis("Vertical") * speed;
+
+        transform.position = new Vector3(horizontal, .5f, vertical);
+
+
+    }
+
+}
---
+++ Assets/Scripts/Player_Stats.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Player_Stats : ScriptableObject
+{
+    public float health = 100.0f;
+    public float speed;
+    public float max_Speed = 10.0f;
+    public float acceleration = .5f;
+
+    public Vector3 position;
+}
